39c6a0090cfbb4ac3587eeb03dd8e84fe74e97c8
Hellen0315 <maryavril315@gmail.com>
2020-12-09 22:12:47 -0500

    inicio
    inicio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{Component} from 'react'
+import tarjeta from './img/webjuju.png'
+import video from './video/juju.mp4'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends Component{
+  render () {
+    return (
+      <div className='App'>
+      <video src={video} width='20%'  height='20%' controls ></video>
+     <img className="imagen" src={tarjeta} />
     </div>
-  );
+    )
+  }
 }
+
 
 export default App;
